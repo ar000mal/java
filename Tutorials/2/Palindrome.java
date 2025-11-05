229b7cff9a9799d7e10e3850b69c010d357022c7
ar00mal <aromalprasad87@gmail.com>
2025-11-05 15:25:54 +0530

    Add Palindrome class for checking if a string is a palindrome

---
+++ Tutorials/2/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String str = sc.nextLine();
+        String rev = "";
+
+        for(int i=str.length()-1 ; i>=0 ; i--){
+            rev += str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println(str + " is a palindrome.");
+        }
+        else{
+            System.out.println(str + " is not a palindorme.");
+        }
+        sc.close();
+    }
+}
